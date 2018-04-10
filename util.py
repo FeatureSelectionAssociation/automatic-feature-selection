7a9e92a1da60ffc07e9dec8b55385531bc4b0bd2
Ivan García <ivanx.garcia88@gmail.com>
2018-04-10 18:20:55 -0500

    Updating util and common file

--- util.py
+++ util.py
@@ -1,21 +1,55 @@
-import copy
+def maxlist(a,b):
+	result = []
+	for i in range(0,len(a)):
+		result.append(max(a[i],b[i]))
+	return result
 
+def getOrderRank(xlist):
+	rank = [i[0] for i in sorted(enumerate(xlist), key=lambda x:x[1])]
+	rank.reverse()
+	#print rank
+	return rank
 
-def findInList(mlist, mvar):
-	for i in range(0,len(mlist)):
-		if(mlist[i]==mvar):	return i
-	return False
+def printresult(PMDbest,PMD2best,labelcol):
+	for i in range(0,len(PMDbest)):
+		print i,labelcol,":",PMDbest[i],PMD2best[i]
 
-def removeColumnFromList(mlist,column):
-	return [c.pop(column) for c in mlist]
-	
-def copyList(mlist):
-	return mlist
+def splitSize(size,split, zero=True):
+	split = int(split)
+	bins = [size]*split
+	binsSize = float(size)/split
+	for i in range(0,split):
+		bins[i] = int(round(binsSize*(i)))
+	bins.append(size)
+	if(zero==False):
+		bins.remove(0)
+	return bins
 
-def removeColumnByName(data,label,removeHeader=1):
-	indexLabel = findInList(data[0],label)
-	if(removeHeader): data.pop(0)
-	return removeColumnFromList(data,indexLabel)
+def intervalSplit(infLim,supLim,split=2,limitIncluded=False):
+	bins = []
+	print "[",infLim, ",", supLim, "]"
+	if(limitIncluded):
+		stepSize = (supLim-infLim)/(split-1.0)
+		addValue = infLim
+		while(addValue <= supLim):
+			bins.append(int(round(addValue)))
+			addValue = addValue + stepSize
+			#print addValue
+	else:
+		stepSize = (supLim-infLim)/(split+1.0)
+		addValue = infLim
+		while(addValue + stepSize < supLim):
+			addValue = addValue + stepSize
+			#print addValue
+			bins.append(int(round(addValue)))
+	#bins = list(set(bins))
+	return bins
 
-def transposeMatrix(mlist):
-	return map(list, zip(*mlist))
+def maxPosition(l):
+	position = 0
+	maxVal = 0
+	for i in range(len(l)):
+		if(l[i]>maxVal):
+			maxVal = l[i]
+			position = i
+	return position
