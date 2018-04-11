7e06b1e135dbf1f5053b86b0f8b71a1ca1f53bcd
Ivan García <ivanx.garcia88@gmail.com>
2018-04-11 10:04:00 -0500

    Adding step based selection for bins

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 import classifiers as cf
 import cuts
 import binDynamic as bd
+import binCreator as bc
 from pandas import *
 import plots as pt
 
@@ -9,30 +10,33 @@ def artificialTest():
 	files = ['simpleCorrelations.csv']
 	buenos = [[1,2,3,4,5,6]]
 	i=0
+	verboseClassifiers = True
 	for f in files:
 		filename = 'Data/'+f
+		###Separate Data
 		print filename, buenos[i]
 		data = read_csv(filename)
 		X = np.array(data.ix[:,0:-1])
-		#X = np.array(data.ix[:])
 		y = np.array(data.ix[:,-1])
 		print X.shape
 		print y.shape
-		#bd.binarySearchBins(X,y,1,5)
-		bd.cuadratureSearchBins(X)
-		#X = np.array(data.ix[:,0:-1])
-		#y = np.array(data.ix[:,-1])
-		#print cf.getBestClassifiers(X,y,verboseClassifiers)
-		##print "static"
-		##fs.get_bestcorlab(xbinset, ybinset, data)
-		##print "dynamic"
-		#[rank,weight] = fs.get_bestcorlabDynamic(data)
+
+		### Search
+		#Dynamic search
+		#bx = bd.binarySearchBins(X,y,1,5)
+		#print bx
+		#bx = bd.cuadratureSearchBins(X)	
+		#print bx
+		#Static search
+		print bc.binsStepBased(data)
+
+		### Cuts
 		#[X,y] = cuts.greatestDiff(rank,weight,data)
-		#print cf.getBestClassifiers(X,y,verboseClassifiers)
 		#[X,y] = cuts.monotonicValidationCut(rank,weight,data)
+		#[X,y] = cuts.fullValidationCut(rank,weight,data)
+		
+		#Classify
 		#print cf.getBestClassifiers(X,y,verboseClassifiers)
-		##[X,y] = cuts.fullValidationCut(rank,weight,data)
-		##print cf.getBestClassifiers(X,y,verboseClassifiers)
 		i = i+1
 		print "-------------------------------------\n"
 
