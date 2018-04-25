f860f5d0e457548daf074f65c4cb5e722e23f79e
AlexLlamas <almas12345678>
2018-04-25 17:33:48 -0500

    Fixed - wrong cmd and umd bin reference in CorrelationMeasures

--- CorrelationMesures.py
+++ CorrelationMesures.py
@@ -129,7 +129,7 @@ def ud_ixy(x, y, num_bin_x, num_bin_y):
 # --------------------------------------- Uniform mutual dependency ---------------------------------------------------
 def umd(x, y, num_bin_x, num_bin_y):
     ixy = ud_ixy(x, y, num_bin_x, num_bin_y)
-    iyx = ud_ixy(y, x, num_bin_x, num_bin_y)
+    iyx = ud_ixy(y, x, num_bin_y, num_bin_x)
     return (ixy + iyx) / 2
 
 
@@ -247,7 +247,6 @@ def cd_ixy(x, y, num_bin_x, num_bin_y):
 # ----------------------------------------- Comparative Mutual Dependency ---------------------------------------------
 def cmd(x, y, num_bin_x, num_bin_y):
     ixy = cd_ixy(x, y, num_bin_x, num_bin_y)
-    iyx = cd_ixy(y, x, num_bin_x, num_bin_y)
+    iyx = cd_ixy(y, x, num_bin_y, num_bin_x)
     return (ixy + iyx) / 2
-
 # ---------------------------------------------------------------------------------------------------------------------
